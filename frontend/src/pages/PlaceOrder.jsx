c2e831b442d2d732fdf91fa00e9b487d3ff97685
[Mayank Patel] <[pm132061@gmail.com]>
2025-04-14 12:09:47 +0530

    some change

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -96,7 +96,11 @@ const PlaceOrder = () => {
 			switch (method) {
 				// API Calls for COD
 				case "cod":
-					const response = await axios.post(backendUrl + "/api/order/place",orderData,{ headers: { token } });
+					const response = await axios.post(
+						backendUrl + "/api/order/place",
+						orderData,
+						{ headers: { token } }
+					);
 					if (response.data.success) {
 						setCartItems({});
 						navigate("/orders");
@@ -119,17 +123,17 @@ const PlaceOrder = () => {
 					}
 					break;
 
-				case "razorpay":
-					const responseRazorpay = await axios.post(
-						backendUrl + "/api/order/razorpay",
-						orderData,
-						{ headers: { token } }
-					);
-					if (responseRazorpay.data.success) {
-						initPay(responseRazorpay.data.order);
-					}
+				// case "razorpay":
+				// 	const responseRazorpay = await axios.post(
+				// 		backendUrl + "/api/order/razorpay",
+				// 		orderData,
+				// 		{ headers: { token } }
+				// 	);
+				// 	if (responseRazorpay.data.success) {
+				// 		initPay(responseRazorpay.data.order);
+				// 	}
 
-					break;
+				// 	break;
 
 				default:
 					break;
@@ -253,12 +257,13 @@ const PlaceOrder = () => {
 							className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
 						>
 							<p
-								className={`min-w-3.5 h-3.5 border rounded-full ${method === "stripe" ? "bg-green-400" : ""
-									}`}
+								className={`min-w-3.5 h-3.5 border rounded-full ${
+									method === "stripe" ? "bg-green-400" : ""
+								}`}
 							></p>
 							<img className="h-5 mx-4" src={assets.stripe_logo} alt="" />
 						</div>
-						<div
+						{/* <div
 							onClick={() => setMethod("razorpay")}
 							className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
 						>
@@ -267,14 +272,15 @@ const PlaceOrder = () => {
 									}`}
 							></p>
 							<img className="h-5 mx-4" src={assets.razorpay_logo} alt="" />
-						</div>
+						</div> */}
 						<div
 							onClick={() => setMethod("cod")}
 							className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
 						>
 							<p
-								className={`min-w-3.5 h-3.5 border rounded-full ${method === "cod" ? "bg-green-400" : ""
-									}`}
+								className={`min-w-3.5 h-3.5 border rounded-full ${
+									method === "cod" ? "bg-green-400" : ""
+								}`}
 							></p>
 							<p className="text-gray-500 text-sm font-medium mx-4">
 								CASH ON DELIVERY
